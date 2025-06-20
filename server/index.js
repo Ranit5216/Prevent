aa31818c89427c8de1faeedd6a23b605eaf4c9c9
Ranit5216 <ranitgiri8145@gmail.com>
2025-06-21 00:35:22 +0530

    new change1

--- server/index.js
+++ server/index.js
@@ -15,42 +15,39 @@ import cartRouter from './route/cart.route.js'
 import addressRouter from './route/address.route.js'
 import orderRouter from './route/order.route.js'
 
-
 const app = express()
 app.use(cors({
-    credentials : true,
-    origin : process.env.FRONTEND_URL
+    credentials: true,
+    origin: process.env.FRONTEND_URL
 }))
 app.use(express.json())
 app.use(cookieParser())
 // app.use(morgan())
 app.use(helmet({
-    crossOriginResourcePolicy : false
+    crossOriginResourcePolicy: false
 }))
 
-const PORT = 8080 || process.env.PORT
+const PORT = process.env.PORT || 8080
 
-app.get("/",(request,response)=>{
-    ///server to client
+app.get("/", (request, response) => {
     response.json({
-        message : "Server is running" + PORT
+        message: "Server is running " + PORT
     })
 })
 
-app.use('/api/user',userRouter)
-app.use('/api/category',categoryRouter)
-app.use('/api/file',uploadRouter)
-app.use("/api/subcategory",subCategoryRouter)
-app.use("/api/product",productRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/address",addressRouter)
-app.use("/api/order",orderRouter)
+app.use('/api/user', userRouter)
+app.use('/api/category', categoryRouter)
+app.use('/api/file', uploadRouter)
+app.use("/api/subcategory", subCategoryRouter)
+app.use("/api/product", productRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/address", addressRouter)
+app.use("/api/order", orderRouter)
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("Server is running",PORT)
-    })
-    
+connectDB().then(() => {
+    //app.listen(PORT, () => {
+    //    console.log("Server is running", PORT)
+    //})
 })
 
 
